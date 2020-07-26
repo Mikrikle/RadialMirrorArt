6eb9ff294dc4992928ad618c82699e8b8d82b7f4
Miriskrit <miha1502@mail.ru>
2020-07-26 14:44:26 +0300

    simple paint

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line, SmoothLine
 
 
-class PaintWidget(Widget):
-    pass
+class MyPaintWidget(Widget):
+
+    def on_touch_down(self, touch):
+        color = (random(), 1, 1)
+        with self.canvas:
+            Color(*color, mode='hsv')
+            #d = 30.
+            #Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['userline'] = Line(points=(touch.x, touch.y), width=3, cap='round', joint='round', close=False)
+            #touch.ud['line'] = SmoothLine(points=(touch.x, touch.y), width=3)
+
+    def on_touch_move(self, touch):
+        touch.ud['userline'].points += [touch.x, touch.y]
 
 
 class MyPaintApp(App):
+
     def build(self):
-        return PaintWidget()
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 
 if __name__ == '__main__':
